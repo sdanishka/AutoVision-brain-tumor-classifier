04a96e9ab0c6c245800fe3839b60c2e5d465a4ee
Danish Afridi <76814231+sdanishka@users.noreply.github.com>
2024-08-06 15:29:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,63 @@
 import streamlit as st
-from inference_sdk import InferenceHTTPClient
+from tensorflow import keras
 from PIL import Image
-import io
+import numpy as np
 
-# Initialize the InferenceHTTPClient
-CLIENT = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key="Pqs2Di4XkKfl9U7avfkn"
-)
+# Load your Keras model
+model = keras.models.load_model('saved_models/trained.h5')
 
-def infer_image(image):
-    # Convert image to bytes
-    buffer = io.BytesIO()
-    image.save(buffer, format="JPEG")
-    buffer.seek(0)
-    result = CLIENT.infer(buffer, model_id="brain-tumors-detection/2")
+def make_predictions(image):
+    # Resize the image for the model
+    img_d = image.resize((244, 244))
+
+    # Check if the image is RGB or not
+    if len(np.array(img_d).shape) < 3:
+        rgb_img = Image.new("RGB", img_d.size)
+        rgb_img.paste(img_d)
+    else:
+        rgb_img = img_d
+
+    # Convert the image into a numpy array and reshape
+    rgb_img = np.array(rgb_img, dtype=np.float64)
+    rgb_img = rgb_img.reshape(1, 244, 244, 3)
+
+    # Make predictions
+    predictions = model.predict(rgb_img)
+    confidence = np.max(predictions)
+    a = int(np.argmax(predictions))
+    
+    # Set a confidence threshold
+    confidence_threshold = 0.5  # You can adjust this threshold
+
+    if confidence < confidence_threshold:
+        return "The uploaded image might not be a valid brain X-ray."
+    
+    if a == 1:
+        result = "Result: Glioma Tumor"
+    elif a == 2:
+        result = "Result: Meningioma Tumor"
+    elif a == 3:
+        result = "Result: No Tumor"
+    else:
+        result = "Result: Pituitary Tumor"
+    
     return result
 
-st.title("Brain Tumor Detection")
+# Streamlit UI
+st.title("Brain Tumor Classification")
 
-# Upload image
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Read and display the image
+    # Load the uploaded image
     image = Image.open(uploaded_file)
+
+    # Display the uploaded image
     st.image(image, caption='Uploaded Image', use_column_width=True)
-    
-    # Perform inference
-    st.write("Classifying...")
-    try:
-        result = infer_image(image)
-        # Display results
-        st.write("Results:")
-        st.json(result)
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+
+    st.write("")
+
+    # Predict button
+    if st.button("Predict"):
+        predictions = make_predictions(image)
+        st.write(predictions)
